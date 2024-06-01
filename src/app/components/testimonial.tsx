eb4d797ac0f1f9eb6add9226d65c116eb355e2d7
Anuujin Purev <amoramgl@gmail.com>
2024-06-01 14:06:38 +0800

    style fixed

--- src/app/components/testimonial.tsx
+++ src/app/components/testimonial.tsx
@@ -34,11 +34,11 @@ export function InfiniteMovingCardsDemo() {
       ];
       
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="h-[40vh] flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={testimonials}
         direction="left"
-        speed="slow"
+        speed="fast"
         className="bg-gray-950"
       />
     </div>
